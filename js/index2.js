742e91b7ca0288371b5e98f610dcba7316c131b9
GermanA <armendariz.german@gmail.com>
2021-11-06 20:39:12 -0600

    Listas Desplegables

---
+++ js/index2.js
@@ -0,0 +1,33 @@
+/* Cargar Ciudad */
+
+$(document).ready(function(){
+    $.ajax({
+      type: 'POST',
+      url: 'php/CargarCiudad.php'
+    })
+  
+    .done(function(listas_rep){
+      $('#selectCiudad').html( listas_rep)
+    })
+    .fail(function(){
+      alert('Hubo un errror al cargar la CIUDAD')
+    })
+   
+  })
+
+  /* Cargar Tipo */
+
+  $(document).ready(function(){
+    $.ajax({
+      type: 'POST',
+      url: 'php/CargarTipo.php'
+    })
+  
+    .done(function(listas_rep){
+      $('#selectTipo').html( listas_rep)
+    })
+    .fail(function(){
+      alert('Hubo un errror al cargar la Tipo')
+    })
+   
+  })
---
+++ php/CargarTipo.php
@@ -0,0 +1,51 @@
+<?php
+
+
+function getListasRep()
+{
+  $parametro = "Tipo";
+  $data = file_get_contents("../data-1.json");
+  $products = json_decode($data, true);
+
+  function unique_multidim_array($array, $key)
+  {
+    $temp_array = array();
+    $i = 0;
+    $key_array = array();
+
+    foreach ($array as $val) {
+      if (!in_array($val[$key], $key_array)) {
+        $key_array[$i] = $val[$key];
+        $temp_array[$i] = $val;
+      }
+
+      $i++;
+    }
+    return $temp_array;
+  }
+
+  $details = unique_multidim_array($products, $parametro);
+
+
+  $b = array_values($details);
+
+  $i = 0;
+
+  $listas = '<option value="0">Elige un Tipo </option>';
+
+
+  foreach ($b as $bb) {
+    //var_dump($b[$i]["Ciudad"]);
+    //$c = array($b[$i]["Ciudad"]);
+      $c= $b[$i]["Tipo"];
+    //var_dump($c);
+    $listas .= "<option value='$c'>$c</option>";
+    $i++;
+  };
+
+
+
+  return $listas;
+};
+
+echo getListasRep();
